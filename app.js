da7d1141e1a82498875407762fb2f896df74c3b9
tyler <tylertooxclusive@gmail.com>
2021-09-30 23:52:07 +0100

    icon changes

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ let weatherDescription = document.querySelectorAll('#weather_Desc')
 const icons = document.querySelectorAll('#icon')  
 const weatherToday = document.querySelector('#today_date')
 const temperature_Today = document.querySelector('#tempOne')
-let condition = []
-let desc = []
+const daysOfWeek = document.querySelectorAll('#determine_day')
 
  form.addEventListener('submit', (e) => {
        e.preventDefault()
@@ -22,30 +21,21 @@ let desc = []
       }).then(data => {
 
         // get array of days from Data // 
-        const listDays = data.list
-         
+        const daysData = data.list
+
         //get day 2 to 7 //
-        const fiveDays = listDays.slice(1,7)
-        const [one, two, three, four, five, six] = fiveDays
+        const sixFollowingDays = daysData.slice(1,7)
+        const [one, two, three, four, five, six] = sixFollowingDays
             
         // Weather and Icon For Present Day //
-
-        let todayTemp = listDays[0].temp.day
-        let todayWeather = listDays[0].weather[0].main
-
+        let todayTemp = Math.floor(daysData[0].temp.day)
+        dayWeather.innerHTML = `
+         <img src="http://openweathermap.org/img/wn/${daysData[0].weather[0].icon}@2x.png">
+        `
         // Add Input value to Html and temperature for Present day // 
         weatherToday.innerHTML = ` In ${inputLocation.value}`
-        temperature_Today.textContent = `${todayTemp} C`
+        temperature_Today.textContent = `${todayTemp}°C`  
      
-        // Condition for Icon for Present Day //
-        if(todayWeather === 'Rain'){
-          dayWeather.classList.add('rain')
-        } else if(todayWeather === 'Clouds'){
-          dayWeather.classList.add('cloudy')
-        } else if(todayWeather === 'Clear'){
-          dayWeather.classList.add('sunny')
-        } 
-
         // Call Day of The week function // 
         dayOfWeek()
 
@@ -53,139 +43,71 @@ let desc = []
         forecast.forEach((cast, index) => {
               switch(index){
                 case 0:
-                  cast.textContent = `${one.temp.day}  C` 
+                  cast.textContent = Math.floor(one.temp.day) + '°C'  
                 break;
                 case 1:
-                  cast.textContent = `${two.temp.day} C`
+                  cast.textContent = Math.floor(two.temp.day) + '°C'  
                 break;
                 case 2:
-                  cast.textContent = `${three.temp.day} C`
+                  cast.textContent = Math.floor(three.temp.day) + '°C'  
                 break;
                 case 3:
-                  cast.textContent = `${four.temp.day} C`
+                  cast.textContent = Math.floor(four.temp.day) + '°C'  
                 break;
                 case 4:
-                  cast.textContent = `${five.temp.day} C`
+                  cast.textContent = Math.floor(five.temp.day) + '°C'  
                 break;
                 case 5:
-                  cast.textContent = `${six.temp.day} C`
+                  cast.textContent = Math.floor(six.temp.day) + '°C'  
                 break;
               }
         })
-         
 
-        let condition = []
-        let desc = []
-  
-        for(let main of fiveDays){
-          let weather = main.weather
-          for(let weathers of weather) {
-            condition.push(weathers.main)
-            desc.push(weathers.description)
-         }
-        }
-         
-  
 
         weatherDescription.forEach((mainDesc,  index) => {
             switch(index){
                case 0:
-                mainDesc.innerHTML = `(${desc[0]})`
+                mainDesc.innerHTML = `(${one.weather[0].description})`
                break;
                case 1:
-                mainDesc.innerHTML = `(${desc[1]})`
+                mainDesc.innerHTML = `(${two.weather[0].description})`
                break;
                case 2:
-                mainDesc.innerHTML = `(${desc[2]})`
+                mainDesc.innerHTML = `(${three.weather[0].description})`
                break;
                case 3:
-                mainDesc.innerHTML = `(${desc[3]})`
+                mainDesc.innerHTML = `(${four.weather[0].description})`
                break;
                case 4:
-                mainDesc.innerHTML = `(${desc[4]})`
+                mainDesc.innerHTML = `(${five.weather[0].description})`
                break;
                case 5:
-                mainDesc.innerHTML = `(${desc[5]})`
+                mainDesc.innerHTML = `(${six.weather[0].description})`
                break;
             }
          })
 
         icons.forEach((icon, index) => {
-            //day one //
             if(index === 0){
-              if(condition[0] === 'Rain'){
-                  icon.classList.add('rain')
-                } else if(condition[0] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[0] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+             icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${one.weather[0].icon}@2x.png">`
             }
-  
-            //day two//
             if(index === 1){
-              if(condition[1] === 'Rain'){
-                  icon.classList.add('rain')
-                } else if(condition[1] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[1] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+              icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${two.weather[0].icon}@2x.png">`            
             }
-  
-            //day three //
-  
             if(index === 2){
-              if(condition[2] === 'Rain'){
-                  icon.classList.add('rain')
-                 } else if(condition[2] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[2] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+              icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${three.weather[0].icon}@2x.png">`   
             }
-  
-             //day four //
-  
-             if(index === 3){
-              if(condition[3] === 'Rain'){
-                  icon.classList.add('rain')
-                } else if(condition[3] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[3] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+            if(index === 3){
+              icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${four.weather[0].icon}@2x.png"> `   
             }
-
-              //day five //
-  
-             if(index === 4){
-              if(condition[4] === 'Rain'){
-                  icon.classList.add('rain')
-                 } else if(condition[4] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[4] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+            if(index === 4){
+              icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png">`   
             }
-  
-             //day six //
-  
-             if(index === 5){
-              if(condition[5] === 'Rain'){
-                  icon.classList.add('rain')
-                } else if(condition[5] === 'Clouds'){
-                  icon.classList.add('cloudy')
-                } else if(condition[5] === 'Clear'){
-                  icon.classList.add('sunny')
-                } 
+            if(index === 5){
+              icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${six.weather[0].icon}@2x.png">`   
             }
-
          })
-
-         inputLocation.value = ''
-         
-
+         inputLocation.value = ''  
       }).catch(error => {
         pasteError.textContent = error
         });
@@ -196,32 +118,48 @@ let desc = []
   // Day of The Week function //
   function dayOfWeek() {
     let d = new Date();
-    let weekday = ['','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    const daysOfWeek = document.querySelectorAll('#determine_day')
+    let weekday = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+  
     daysOfWeek.forEach((day, index) =>{
        switch(index){
           case 0:
-            day.innerHTML = weekday[d.getDay()+1]
+            day.innerHTML = weekday[d.getDay()]
           break;
           case 1:
-            day.innerHTML = weekday[d.getDay()+2]
+            day.innerHTML = weekday[d.getDay()+1]
           break;
           case 2:
-            day.innerHTML = weekday[d.getDay()+3]
+            day.innerHTML = weekday[d.getDay()+2]
           break; 
           case 3:
-            day.innerHTML = weekday[d.getDay()+3]
-          break; 
-          case 4:
             day.innerHTML = weekday[d.getDay()-2]
           break; 
+          case 4:
+            day.innerHTML = weekday[d.getDay()]
+          break; 
           case 5:
-            day.innerHTML = weekday[d.getDay()-1]
+            day.innerHTML = weekday[d.getDay()]
           break; 
        }
     })
   }
 
- 
 
-  
+function addDays(dateObj, numDays) {
+    dateObj.setDate(dateObj.getDate() + numDays);
+    return dateObj;
+ }
+
+ let now = new Date();
+ var tomorrow = addDays(new Date(), 1);
+ var nextWeek = addDays(new Date(), 7);
+
+ console.log(nextWeek.toLocaleDateString('en-US',{ weekday:'long'}))
+ 
+console.log(
+     'Today: ' + now +
+     '\nTomorrow: ' + tomorrow +
+     '\nNext week: ' + nextWeek
+ );
+
+ 
